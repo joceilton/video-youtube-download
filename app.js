12dd9197d654cc84ef8b341be0bfa8485cb14c9c
joceilton <joceilton.g@gmail.com>
2022-01-13 12:17:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
-const download = require('./video_download')
+//const download = require('./video_download')
 
 app.set('view engine', 'ejs')
 app.set('views', 'src')
@@ -11,10 +11,11 @@ app.use(express.static('src/public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-	res.render('index');
+	//res.render('index');
+          res.send("ok")
 })
 
-app.post('/download', async (req, res) => {
+/*app.post('/download', async (req, res) => {
 	var execute = await download.baixarVideo(res, req.body.url, "mp4")
 	console.log(execute)
 })
@@ -26,7 +27,7 @@ app.get('/concluido', (req,res) => {
 app.get('/video/:video_url', (req, res) => {
 	var video_url = req.params.video_url
 	res.sendFile(__dirname + "/src/video/" + video_url)
-})
+})*/
 
 app.listen(3000, ()=> {
 	console.log('Servidor inciado na porta 3000')
